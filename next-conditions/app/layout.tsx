0300f5e6706fd07ac6dbef266988c2774d77946d
Flexerr22 <ilia2005951@gmail.com>
2024-12-10 00:01:45 +0300

    Создал проект next conditions и добавил в него все нужные данные для начала разработки

---
+++ next-conditions/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import {Nunito} from 'next/font/google';
+
+const nunito = Nunito({
+  subsets: ['cyrillic'],
+  variable: '--font-nunito',
+  weight: ['400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: "Next Conditions | Главная",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={nunito.variable}>
+          <main className="min-h-screen">
+          {children}
+          </main>
+      </body>
+    </html>
+  );
+}
